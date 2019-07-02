f6b7a07e74c1785f3181ed3fb7338b4c1db9e689
David Power <david.power@wpengine.com>
2019-07-02 19:30:55 +0100

    Adding logic and test structure

---
+++ projects/unittesting_1/app/fancyText.py
@@ -0,0 +1,3 @@
+class FancyText:
+    def snakecase(self, text_to_be_formatted):
+        return text_to_be_formatted
---
+++ projects/unittesting_1/tests/test_fancy_text.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestFancyText(unittest.TestCase):
+
+    def test_upper_and_lower_case_a_uppercase_word(self):
+        return 
+        
+        
