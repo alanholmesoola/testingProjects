31469aec56db59902ee3ee15f5ac3f880d2ff6fb
David Power <david.power@wpengine.com>
2019-07-02 20:02:11 +0100

    testing

--- projects/unittesting_1/test/test_fancy_text.py
+++ projects/unittesting_1/test/test_fancy_text.py
@@ -1,8 +1,17 @@
+from app.fancyText import FancyText
+
 import unittest
 
 class TestFancyText(unittest.TestCase):
 
     def test_upper_and_lower_case_a_uppercase_word(self):
-        return 
+        # Arrange
+        fancy = FancyText()
+        test_data = "david"
+        expected_result = "DaViD"
         
+        # Act
+        observed = fancy.mixed_case(test_data)
         
+        # Assert
+        self.assertEqual(observed, expected_result)
