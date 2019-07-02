88fef91ad64f3bd50330ec6fb03ea17269dccf40
Alan Holmes <alanholmesoola@yahoo.ie>
2019-07-02 19:39:56 +0100

    Added mixed_case funtion

--- projects/unittesting_1/app/fancyText.py
+++ projects/unittesting_1/app/fancyText.py
@@ -1,3 +1,18 @@
 class FancyText:
-    def snakecase(self, text_to_be_formatted):
-        return text_to_be_formatted
+    def mixed_case(self, text_to_be_formatted):
+        index = 0
+        mystring = ""
+    
+        for item in text_to_be_formatted:
+
+            if index%2 == 0:
+                item = item.upper()
+                mystring = mystring+item
+                index = index+1
+            
+            elif index%2 != 0:
+                item = item.lower()
+                mystring = mystring+item  
+                index = index+1
+            
+        return mystring
