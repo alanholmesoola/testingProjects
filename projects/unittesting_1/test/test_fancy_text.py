1fde5550d55d118ea604f91c855745aa60ee98e0
Alan Holmes <alanholmesoola@yahoo.ie>
2019-07-02 20:19:05 +0100

    tests

--- projects/unittesting_1/test/test_fancy_text.py
+++ projects/unittesting_1/test/test_fancy_text.py
@@ -7,7 +7,7 @@ class TestFancyText(unittest.TestCase):
     def test_upper_and_lower_case_a_uppercase_word(self):
         # Arrange
         fancy = FancyText()
-        test_data = "david"
+        test_data = "DAVID"
         expected_result = "DaViD"
         
         # Act
@@ -15,3 +15,16 @@ class TestFancyText(unittest.TestCase):
         
         # Assert
         self.assertEqual(observed, expected_result)
+
+
+    def test_upper_and_lower_case_a_sentence(self):
+        #Arrange
+        fancy = FancyText()
+        test_data = "alan sure is great"
+        expected_result = "AlAn sUrE Is gReAt"
+
+        #Act
+        observed = fancy.mixed_case(test_data)
+
+        #Assert
+        self.assertEqual(observed,expected_result)
